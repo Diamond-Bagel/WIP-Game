436e6a34e216c72d9a3e3a7d0d65631abf009858
Diamond-Bagel <72178852+Diamond-Bagel@users.noreply.github.com>
2020-10-13 17:15:11 -0700

    Add files via upload
    The basic map for the game. There will be an (as of right now) six areas that the player can visit.

---
+++ map.py
@@ -0,0 +1,21 @@
+map = {
+    "Home": {
+        "DESCRIPTION": 'Your comfy home',
+        "REGULAR ITEM POOL": ['Apple', 'Photo', 'Fork',
+                              'Battle Spoon', 'Father\'s Shield']
+    },
+
+    "Church": {
+        "DESCRIPTION": 'A church of the Supreme God',
+        "REGULAR ITEM POOL": ['Holy Cross'],
+        "SPECIAL ITEM POOL": ['Holy Necklace of the Supreme God']
+    },
+
+    "Town": {
+        "DESCRIPTION": 'The town you grew up in',
+    },
+
+    "Arena": {
+        "DESCRIPTION": 'The town\'s local arena. \nBattles between monsters and adventurers are held here.'
+    }
+}
